a40238dff77dfab013901ccece1391362518ea8f
Andrei Stanimir <andreistanimir@live.com>
2025-12-13 21:57:29 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,7 +12,9 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		command = strings.TrimRight(command, "\n")
-
+		if command == "exit" {
+			return
+		}
 		fmt.Fprintf(os.Stdout, "%s: command not found", command)
 		fmt.Fprint(os.Stdout, "\n")
 	}
