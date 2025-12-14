3ce08c050b7a017a869e3d99a6167da7c7ce9e77
Andrei Stanimir <andreistanimir@live.com>
2025-12-14 18:43:44 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,11 +4,27 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
+func IsExecAny(mode os.FileMode) bool {
+	return mode&0o111 != 0
+}
+
+func FilePathWalkDir(root string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
+}
+
 func main() {
-	for true {
+	for {
 		_, err := fmt.Fprint(os.Stdout, "$ ")
 		if err != nil {
 			return
@@ -29,7 +45,35 @@ func main() {
 			case "exit", "echo", "type":
 				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", rest[0])
 			default:
-				fmt.Fprintf(os.Stdout, "%s: not found\n", rest[0])
+				PATH := os.Getenv("PATH")
+				found := false
+				for path := range strings.SplitSeq(PATH, ":") {
+					// fmt.Fprintf(os.Stdout, "Searching in %s\n", path)
+
+					files, err := os.ReadDir(path)
+					if err != nil {
+						// fmt.Fprintf(os.Stdout, "%s error: %v\n", path, err)
+						continue
+					}
+					for _, f := range files {
+						// fmt.Fprintf(os.Stdout, "Checking file: %s\n", f.Name())
+						if !f.IsDir() && f.Name() == rest[0] {
+							// fmt.Fprintln(os.Stdout, "file: %s", f.Name())
+							info, _ := f.Info()
+							if IsExecAny(info.Mode()) {
+								fmt.Fprintf(os.Stdout, "%s is %s\n", rest[0], filepath.Join(path, rest[0]))
+								found = true
+								break
+							}
+						}
+					}
+					if found {
+						break
+					}
+				}
+				if !found {
+					fmt.Fprintf(os.Stdout, "%s: not found\n", rest[0])
+				}
 			}
 		default:
 			fmt.Fprintf(os.Stdout, "%s: command not found", command)
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/codecrafters-io/shell-starter-go
 
 go 1.24.0
+
+require (
+	github.com/samber/lo v1.52.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
